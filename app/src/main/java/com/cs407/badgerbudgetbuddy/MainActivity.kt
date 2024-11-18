10fc6a8d014398a381b2b30590874534f54c592d
Waleed Bitar <waleed@avtal.com>
2024-11-17 19:19:34 -0600

    Added menu bar for homepage.

--- app/src/main/java/com/cs407/badgerbudgetbuddy/MainActivity.kt
+++ app/src/main/java/com/cs407/badgerbudgetbuddy/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.cs407.badgerbudgetbuddy
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,41 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    //TODO Below items will need to swap to a different activity
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.bucky -> {
+                Toast.makeText(this, "Hi Badger", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item2 -> {
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.subitem1 -> {
+                Toast.makeText(this, "SubItem 1 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.subitem2 -> {
+                Toast.makeText(this, "SubItem 2 selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
