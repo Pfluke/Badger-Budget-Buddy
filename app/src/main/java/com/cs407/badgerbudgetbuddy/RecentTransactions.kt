8e556e5ed00ae35ac2fdd9216dd1678b3030aa7c
Waleed Bitar <waleed@avtal.com>
2024-12-04 16:09:50 -0600

    Added data class for Transactions

--- app/src/main/java/com/cs407/badgerbudgetbuddy/RecentTransactions.kt
+++ app/src/main/java/com/cs407/badgerbudgetbuddy/RecentTransactions.kt
@@ -13,35 +13,5 @@ class RecentTransactions: BaseActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recent_transactions) // Ensure you have a layout file for this activity
     }
-
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.menu, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return when (item.itemId) {
-//            R.id.bucky -> {
-//                Toast.makeText(this, "Hi Badger!", Toast.LENGTH_SHORT).show()
-//                true
-//            }
-//            // logout of account
-//            R.id.item2 -> {
-//                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
-//                true
-//            }
-//            // To switch to recent transactions
-//            R.id.subitem1 -> {
-//                // Toast.makeText(this, "SubItem 1 selected", Toast.LENGTH_SHORT).show()
-//                val recentTransactionsIntent = Intent(this, RecentTransactions::class.java)
-//                startActivity(recentTransactionsIntent)
-//                true
-//            }
-//            R.id.subitem2 -> {
-//                Toast.makeText(this, "SubItem 2 selected", Toast.LENGTH_SHORT).show()
-//                true
-//            }
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
+    
 }
